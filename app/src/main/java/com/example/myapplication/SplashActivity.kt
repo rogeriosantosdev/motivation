e4e3c7fafaa82d555a131cbc868113ca52081b89
Rogerio Santos <rogeriosantosdev@gmail.com>
2022-02-19 07:44:49 -0300

    splash activity created

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if(supportActionBar != null){
+            supportActionBar!!.hide()
+        }
+    }
+}
