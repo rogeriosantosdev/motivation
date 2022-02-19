7439544601a488ec64a9625220cea56f40a998fd
Rogerio Santos <rogeriosantosdev@gmail.com>
2022-02-19 08:08:24 -0300

    click events

--- app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -1,15 +1,38 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         if(supportActionBar != null){
             supportActionBar!!.hide()
+        }
+
+        buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if(id == R.id.buttonSave){
+            handleSave()
+        }
+    }
+
+    private fun handleSave(){
+        val name = editName.text.toString()
+
+        if(name != ""){
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, "Informe o seu nome", Toast.LENGTH_SHORT).show()
         }
     }
 }
