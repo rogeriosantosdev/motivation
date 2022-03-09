5b9c9b45147a29cff3fb830217ccdd6e954c1d97
Rogerio Santos <rogeriosantosdev@gmail.com>
2022-03-08 23:03:47 -0300

    main activity and onclicklistener

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.myapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.myapplication.R
 import com.example.myapplication.infra.MotivationConstants
 import com.example.myapplication.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mSecurityPreferences: SecurityPreferences
 
@@ -18,6 +19,36 @@ class MainActivity : AppCompatActivity() {
         mSecurityPreferences = SecurityPreferences(this)
         textName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
 
+
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+
+        val listFilter = listOf(R.id.image_all, R.id.image_happy, R.id.image_morning)
+
+        if (id == R.id.buttonNewPhrase) {
+            handleNewPhrase()
+        } else if (id in listFilter) {
+            handleFilter(id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+        when (id) {
+            R.id.image_all -> {
+                image_all.setColorFilter(R.color.colorAccent)
+            }
+            R.id.image_happy -> {
+                image_happy.setColorFilter(R.color.colorAccent)
+            }
+            R.id.image_morning -> {
+                image_morning.setColorFilter(R.color.colorAccent)
+            }
+        }
+    }
+
+    private fun handleNewPhrase() {
 
     }
 }
